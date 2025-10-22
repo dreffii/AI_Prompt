7d44b3c141bcb95066e446006d57593f9e21ec09
dreffii <165928388+dreffii@users.noreply.github.com>
2025-10-23 00:43:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,12 @@ st.sidebar.title("🧭 Navigation")
 
 # Save selected page in session_state
 if "page" not in st.session_state:
-    st.session_state.page = "Prompt Generator"
+    st.session_state.page = "Prompt Generator 🐰"
 
 page = st.sidebar.radio(
     "Go to:",
-    ["Prompt Generator", "Blank Page"],
-    index=0 if st.session_state.page == "Prompt Generator" else 1,
+    ["Prompt Generator 🐰", "Blank Page"],
+    index=0 if st.session_state.page == "Prompt Generator 🐰" else 1,
     label_visibility="collapsed"
 )
 
@@ -26,7 +26,7 @@ st.sidebar.markdown("**Created by Oak Sopheaktra👾**")  # your credit
 # st.sidebar.image("data/IMG_3172.PNG", use_container_width=True)
 
 # --- Load Pages Dynamically ---
-if st.session_state.page == "Prompt Generator":
+if st.session_state.page == "Prompt Generator 🐰":
     from pages.page1 import show_page_1
     show_page_1()
 elif st.session_state.page == "Blank Page":
