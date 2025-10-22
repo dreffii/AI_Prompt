cb4b857e86c73b591d637392beac5ca0b146bd81
dreffii <165928388+dreffii@users.noreply.github.com>
2025-10-23 00:19:06 +0700

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-def show_page2():
+def show_page_2():
     st.set_page_config(page_title="📄 Page 2", layout="wide")
     st.title("Page 2")
     st.write("This page is currently blank. Add content here later.")
+
